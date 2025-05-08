9cc9a438e35482294b50a3305206f93d976fdd8a
Roha Shahid <shahidroha824@gmail.com>
2025-05-08 11:31:40 +0500

    code push

---
+++ src/components/AppSidebar.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import {
+  CCloseButton,
+  CSidebar,
+  CSidebarBrand,
+  CSidebarFooter,
+  CSidebarHeader,
+  CSidebarToggler,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+
+import { AppSidebarNav } from './AppSidebarNav'
+import { sygnet } from 'src/assets/brand/sygnet'
+
+// sidebar nav config
+import navigation from '../_nav'
+
+const AppSidebar = () => {
+  const dispatch = useDispatch()
+  const unfoldable = useSelector((state) => state.sidebarUnfoldable)
+  const sidebarShow = useSelector((state) => state.sidebarShow)
+
+  return (
+    <CSidebar
+      // className="border-end"
+      colorScheme="dark"
+      position="fixed"
+      unfoldable={unfoldable}
+      visible={sidebarShow}
+      onVisibleChange={(visible) => {
+        dispatch({ type: 'set', sidebarShow: visible })
+      }}
+    >
+      <CSidebarHeader>
+        <CSidebarBrand to="/">
+          {/* <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} /> */}
+          <img src='src/assets/images/Logo.png' className='logo' />
+          <CIcon customClassName="sidebar-brand-narrow" icon={sygnet} height={32} />
+        </CSidebarBrand>
+        <CCloseButton
+          className="d-lg-none"
+          dark
+          onClick={() => dispatch({ type: 'set', sidebarShow: false })}
+        />
+      </CSidebarHeader>
+      <AppSidebarNav items={navigation} />
+      <CSidebarFooter className="d-none d-lg-flex">
+        <div className='nav-feature-card'>
+          <div className='d-flex justify-content-between top-icons'>
+            <i className="bi bi-exclamation-triangle warning-icon"></i>
+            <i className="bi bi-x-lg close-icon"></i>
+          </div>
+          <div className='footer-description'>
+            Enjoy unlimited access to our app with only a small price monthly.
+          </div>
+          <div>
+            <a href='/' className='dismiss-anchor'>Dismiss</a>
+            <a href='/' className='pro-anchor'>Go pro</a>
+          </div>
+        </div>
+        <CSidebarToggler
+          onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}
+        />
+      </CSidebarFooter>
+    </CSidebar>
+  )
+}
+
+export default React.memo(AppSidebar)
